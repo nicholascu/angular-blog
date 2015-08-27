b04f345c9e47eb75b71428f34b803044e4e6055d
Nicholas Cu <nicholascu@gmail.com>
2015-08-27 15:27:12 -0700

    Add function to addPost and form to view

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 angular.module("angularBlog", [])
 .controller("MainCtrl", [
 '$scope', 
-function($scope){
-	$scope.test = "HELLO WORLD!"
+	function($scope){
+		$scope.test = "HELLO WORLD!"
 
-	$scope.posts = [
-		{title: 'post 1', upvotes: 5},
-		{title: 'post 2', upvotes: 2},
-		{title: 'post 3', upvotes: 15},
-		{title: 'post 4', upvotes: 9},
-		{title: 'post 5', upvotes: 4}
-	]
-}]);
+		$scope.posts = [
+			{title: 'post 1', upvotes: 5},
+			{title: 'post 2', upvotes: 2},
+			{title: 'post 3', upvotes: 15},
+			{title: 'post 4', upvotes: 9},
+			{title: 'post 5', upvotes: 4}
+		]
+
+		$scope.addPost = function(){
+			if(!$scope.title || $scope.title ===''){ return;} // prevent blank title
+			$scope.posts.push({title: $scope.title, upvotes: 0});
+			$scope.title = ''; // clears title for reuse of function
+		}
+	} // scope function
+]); // controller
